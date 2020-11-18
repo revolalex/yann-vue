695db1b53d9d8c66967348fa15dfb69a9b59d570
Alex <revolalex@gmail.com>
2020-11-18 21:15:05 +0100

    work on navbar for active link

--- src/components/Global/Navbar.vue
+++ src/components/Global/Navbar.vue
@@ -1,26 +1,68 @@
 <template>
-    <div>
-      <b-navbar toggleable="lg" type="dark" variant="secondary">
-        <b-navbar-brand id="ycLogo" href="/">Yann Crochet</b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="/biographie">Biographie</b-nav-item>
-            <b-nav-item href="/actualites">Actualités</b-nav-item>
-            <b-nav-item href="/galeries">Galeries</b-nav-item>
-            <b-nav-item-dropdown text="Galeries" right>
-              <b-dropdown-item href="/galeries/foret">L'appel de la forêt</b-dropdown-item>
-              <b-dropdown-item href="/galeries/microcosmos">Microcosmos</b-dropdown-item>
-              <b-dropdown-item href="/galeries/vegetale">Féérie végétale</b-dropdown-item>
-              <b-dropdown-item href="/galeries/eau">Au fil de l'eau</b-dropdown-item>
-              <b-dropdown-item href="/galeries/jardin">Jardin d'Eden</b-dropdown-item>
-              <b-dropdown-item href="/galeries/noir">Noir et blanc</b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item href="#">Contact</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="secondary">
+      <b-navbar-brand id="ycLogo" :to="{ name: 'Home' }"
+        >Yann Crochet</b-navbar-brand
+      >
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <router-link
+            to="Biographie"
+            class="nav-item nav-link"
+            active-class="active"
+            exact
+          >
+            Biographie
+          </router-link>
+          <router-link
+            to="Actualites"
+            class="nav-item nav-link"
+            active-class="active"
+            exact
+          >
+            Actualites
+          </router-link>
+          <router-link
+            to="Galeries"
+            class="nav-item nav-link"
+            active-class="active"
+            exact
+          >
+            Galeries
+          </router-link>
+          <b-nav-item-dropdown text="Galeries" right>
+            <b-dropdown-item :to="{ name: 'Foret' }"
+              >L'appel de la forêt</b-dropdown-item
+            >
+            <b-dropdown-item :to="{ name: 'Microcosmos' }"
+              >Microcosmos</b-dropdown-item
+            >
+            <b-dropdown-item :to="{ name: 'Vegetale' }"
+              >Féérie végétale</b-dropdown-item
+            >
+            <b-dropdown-item :to="{ name: 'Eau' }"
+              >Au fil de l'eau</b-dropdown-item
+            >
+            <b-dropdown-item :to="{ name: 'Jardin' }"
+              >Jardin d'Eden</b-dropdown-item
+            >
+            <b-dropdown-item :to="{ name: 'Noir' }"
+              >Noir et blanc</b-dropdown-item
+            >
+          </b-nav-item-dropdown>
+          <router-link
+            to="Contact"
+            class="nav-item nav-link"
+            active-class="active"
+            exact
+          >
+            Contact
+          </router-link>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
