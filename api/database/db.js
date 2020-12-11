93fcc04eb6270e941f3eb6ee5d999252b624eded
Alex <revolalex@gmail.com>
2020-12-11 22:15:07 +0100

    change db

--- api/database/db.js
+++ api/database/db.js
@@ -6,26 +6,35 @@ const connection = mysql.createConnection({
   database: "yann",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected to DB");
 
   let createDB = "CREATE database IF NOT EXISTS yann";
-  connection.query(createDB, function(err, results) {
+  connection.query(createDB, function (err, results) {
     if (err) throw err;
   });
-  // link, category, description, 
+
+  // is_menu, galerie_name, caption, src, alt
   let imgTable =
-    "CREATE TABLE IF NOT EXISTS images (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, link VARCHAR(250) NOT NULL, category VARCHAR(100) NOT NULL,  description VARCHAR(200));";
-  connection.query(imgTable, function(err, results) {
+    "CREATE TABLE IF NOT EXISTS galerie_photo (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, is_menu BOOLEAN, galerie_name VARCHAR(50) NOT NULL, caption VARCHAR(50) NOT NULL,  src VARCHAR(500), alt VARCHAR(50));";
+  connection.query(imgTable, function (err, results) {
     if (err) throw err;
   });
-  // name, password, email
+
+  //name, is_super_admin, password, email
   let adminTable =
-    "CREATE TABLE IF NOT EXISTS admin (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL, password VARCHAR(100) NOT NULL, email VARCHAR(200) NOT NULL);"
-    connection.query(adminTable, function (err, results){
-      if (err) throw err;
-    })
+    "CREATE TABLE IF NOT EXISTS admin (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL, is_super_admin BOOLEAN, password VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL);"
+  connection.query(adminTable, function (err, results) {
+    if (err) throw err;
+  })
+
+  // text, galerie_name, title, date, description
+  let archiveTable =
+    "CREATE TABLE IF NOT EXISTS archive (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, text VARCHAR(1000) NOT NULL, galerie_name VARCHAR(50), title VARCHAR(50) NOT NULL, date DATE NOT NULL, description VARCHAR(50));"
+  connection.query(archiveTable, function (err, results) {
+    if (err) throw err;
+  })
 });
 
 module.exports = connection;
