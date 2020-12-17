3100be4ac58971a31e7d988a40024c7aa3bf175a
Alex <revolalex@gmail.com>
2020-12-17 14:19:56 +0100

    css on photoCard shadow and hover

--- src/components/Photo/PhotoCard.vue
+++ src/components/Photo/PhotoCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="myPhotoContainer">
-    <div v-for="(post, postIndex) in postInPhoto" :key="postIndex" class="padding-bottom">
-      <h4>{{ post.title }}</h4>
+    <div v-for="(post, postIndex) in postInPhoto" :key="postIndex" class="padding-bottom-hover">
+      <h2 id="cardPhotoTitle">{{ post.title }}</h2>
       <b-card id="cardImage" :img-src="imgP(post.filename)">
         <!-- <b-card id="cardImage" :img-src='imgP(post.filename)'> -->
         <b-card-text>{{ post.text }}</b-card-text>
@@ -65,15 +65,31 @@ export default {
 .myPhotoContainer {
   padding: 5% 10% 5% 10%;
 }
+
 #leftText {
   text-align: start;
+}
+#cardPhotoTitle{
+  text-align: center;
+}
+#cardImage{
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 }
 #cardImage img {
   max-height: 700px;
   width: 100%;
   object-fit: scale-down;
 }
-.padding-bottom{
+.padding-bottom-hover{
   padding-bottom: 20vh;
 }
+.padding-bottom-hover:hover{
+  -webkit-transform: scale(1.005);
+  -moz-transform: scale(1.005);
+  transform: scale(1.005);
+}
+#cardImage:hover{
+  box-shadow: 0 20px 40px rgba(0, 0, 0, 0.38), 0 12px 12px rgba(0, 0, 0, 0.46);
+}
+
 </style>
