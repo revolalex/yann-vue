79e0822a6de44c3c5903c187084610629d0ab0d9
Alex <revolalex@gmail.com>
2021-02-03 10:59:34 +0100

    caroussel css

--- src/components/Carousel/Carousel.vue
+++ src/components/Carousel/Carousel.vue
@@ -28,15 +28,36 @@ export default {
      * @param filename
      * @returns the image links
      */
-     getImgSrc(filename) {
-      return  require(`@/assets/uploads/images/caroussel/${filename}`);
+    getImgSrc(filename) {
+      return require(`@/assets/uploads/images/caroussel/${filename}`);
     },
   },
 };
 </script>
 
 <style>
+/* full screen caroussel minus nav */
+.carousel {
+  height: calc(100vh - 56px);
+}
+.carousel-inner,
+.carousel-item {
+  height: 100%;
+}
+.carousel-item {
+  background-color: #000;
+}
+.carousel-item img {
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
 
+/* animation */
+.carousel-inner .carousel-item > img {
+  -webkit-animation: zoom 20s;
+  animation: zoom 20s;
+}
 @-webkit-keyframes zoom {
   from {
     -webkit-transform: scale(1, 1);
@@ -56,19 +77,10 @@ export default {
     transform: scale(1.5, 1.5);
   }
 }
-
-.carousel-inner .carousel-item > img {
-  /* 20s d'origine */
-  -webkit-animation: zoom 20s;
-  animation: zoom 20s;
-}
-
-@media screen and (max-width: 1024px) {
-  /* Specific to this particular image */
+/* @media screen and (max-width: 1024px) {
   .carousel {
     left: 50%;
     margin-left: -512px;
-    /* 50% */
   }
-}
+} */
 </style>
