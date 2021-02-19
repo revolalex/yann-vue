b51fb542d8e48d48a29389876a0cfcc2c1c0195c
Alex <revolalex@gmail.com>
2021-02-19 01:00:35 +0100

    more test

--- tests/unit/Views/Home/Home.spec.js
+++ tests/unit/Views/Home/Home.spec.js
@@ -1,18 +1,18 @@
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import { mount, createLocalVue } from '@vue/test-utils';
-import Home from '@/views/Home/Home.vue'
-import CarouselComponent from "@/components/Carousel/Carousel";
 import VueRouter from 'vue-router'
-import axios from 'axios';
+import Home from '@/views/Home/Home.vue'
+import Carousel from "@/components/Carousel/Carousel.vue";
 
+
+const router = new VueRouter()
 const localVue = createLocalVue();
+localVue.use(VueRouter)
 localVue.use(BootstrapVue);
 localVue.use(IconsPlugin);
-localVue.use(VueRouter)
-const router = new VueRouter()
-jest.mock('axios');
 
-describe('Views Home.vue', () => {
+
+describe('GallerieEau.vue', () => {
     let wrapper
 
     beforeAll(() => {
@@ -22,40 +22,34 @@ describe('Views Home.vue', () => {
             mocks: {
                 $t: (msg) => msg
             },
-            components: {
-                CarouselComponent,
+            components:{
+                Carousel
             },
+            propsData: {
+                imageInCarousel: [
+                    {
+                        src: "01132021130016eau1xs.jpg",
+                        alt: "oiseaux marin",
+                    },
+                    {
+                        src: "01132021130016eau1xs.jpg",
+                        alt: "rapace",
+                    },
+                ],
+            }
+            
         })
     })
 
-    describe('Home', () => {
+    describe('Home View', () => {
+
         it('should be defined', () => {
             expect(wrapper.exists()).toBeTruthy()
         })
-
     })
-
-    test('should fetch photos and show img', async () => {
-        const myImg = [
-            {
-                "id": 1,
-                "filename": "01132021124924foret1xs.jpg"
-            },
-            {
-                "id": 2,
-                "filename": "01132021125312foret24xs.jpg"
-            }]
-        const resp = { status: 200, data: myImg };
-        axios.get.mockImplementation(() => Promise.resolve(resp))
-        wrapper.setData({ myImg: resp.data })
-
-
-    });
-
     afterAll(() => {
         wrapper.destroy()
     })
 
 
 })
-
